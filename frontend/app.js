d9c35de09c0a51becbc427393b46c613ab3f59c0
Dillon Lewis <dillon.lewis@anywhere.re>
2024-07-30 11:44:46 -0400

    Implement keycloak secure nodejs app.

---
+++ frontend/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var stringReplace = require('string-replace-middleware');
+
+var KC_URL = process.env.KC_URL || "http://localhost:8080";
+var SERVICE_URL = process.env.SERVICE_URL || "http://localhost:3000/secured";
+
+var app = express();
+
+app.use(stringReplace({
+    'SERVICE_URL': SERVICE_URL,
+    'KC_URL': KC_URL
+}));
+app.use(express.static('.'))
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.listen(8000, function(){
+    console.log('Started at port 8000');
+});
