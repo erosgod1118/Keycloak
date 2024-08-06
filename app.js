5cea43b597266792539709b7db8279a671ff4f7e
Dillon Lewis <dillon.lewis@anywhere.re>
2024-08-06 13:06:31 -0400

    Implement openid-playground.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var stringReplace = require('string-replace-middleware');
+var cors = require('cors');
+
+var KC_URL = process.env.KC_URL || "http://localhost:8080";
+
+app.use(stringReplace({
+   'KC_URL': KC_URL
+}));
+app.use(express.static('.'))
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+
+app.listen(8000);
